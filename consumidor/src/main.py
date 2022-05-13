cc92e891aeac0433baeda76765be10ec462b9580
Gustavo <ghp2201@gmail.com>
2022-05-12 21:52:29 -0300

    Add main do consumidor

---
+++ consumidor/src/main.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+
+def main():
+    while True:
+        print('Consumindo...')
+        '''
+        TODO - Implementar consumo de dados da API
+        '''
+        sleep(60)
+
+
+if __name__ == '__main__':
+    main()
